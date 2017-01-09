ba47af1882338ecbf8ad6191336de8f1f97d257e
brandy2015 <zhangzihao@zhangzihaodemac-mini.local>
2017-01-09 13:13:09 +0800

    Initial Commit

---
+++ FileReadFromiTunes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FileReadFromiTunes
+//
+//  Created by 张子豪 on 2017/1/9.
+//  Copyright © 2017年 张子豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
